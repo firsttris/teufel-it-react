9beeb9c18596c800dc92c54ce6aa3c6198341aac
firsttris <tristanteufel@googlemail.com>
2017-05-26 21:19:45 +0200

    ignore history plugin

--- src/Impressum.js
+++ src/Impressum.js
@@ -22,16 +22,16 @@ class Impressum extends Component {
           <span>Build with <a href="https://facebook.github.io/react/" target="_blank" rel="noopener noreferrer" style={{ color: '#61dafb', textDecoration: 'none' }}><b>React</b></a></span>
           <img src={Logo} className="App-logo" alt="logo" />
           <div>
-          <div>Quellen für Bilder und Icons:</div>
-          <a href="http://unsplash.com" style={{textDecoration: 'none', color: '#61dafb' }} target="_blank" rel="noopener noreferrer" >unsplash.com </a>
-          <a href="http://shutterstock.com" style={{textDecoration: 'none', color: '#61dafb' }} target="_blank" rel="noopener noreferrer" >shutterstock.com </a>
-          <a href="http://fontawesome.io" style={{textDecoration: 'none', color: '#61dafb' }} target="_blank" rel="noopener noreferrer" >fontawesome.io</a>
+            <div>Quellen für Bilder und Icons:</div>
+            <a href="http://unsplash.com" style={{ textDecoration: 'none', color: '#61dafb' }} target="_blank" rel="noopener noreferrer" >unsplash.com </a>
+            <a href="http://shutterstock.com" style={{ textDecoration: 'none', color: '#61dafb' }} target="_blank" rel="noopener noreferrer" >shutterstock.com </a>
+            <a href="http://fontawesome.io" style={{ textDecoration: 'none', color: '#61dafb' }} target="_blank" rel="noopener noreferrer" >fontawesome.io</a>
           </div>
         </div>
         <div className="col-xs-12 col-md-4 col-lg-4">
           <div>Kontakt:</div>
-          <div><a href="tel:+4917645744166" style={{textDecoration: 'none', color: '#61dafb' }}>+49 (0) 176 457 44 1 66</a></div>
-          <div><a href="mailto:info@teufel-it.de" style={{textDecoration: 'none', color: '#61dafb' }}>info@teufel-it.de</a></div>
+          <div><a href="tel:+4917645744166" style={{ textDecoration: 'none', color: '#61dafb' }}>+49 (0) 176 457 44 1 66</a></div>
+          <div><a href="mailto:info@teufel-it.de" style={{ textDecoration: 'none', color: '#61dafb' }}>info@teufel-it.de</a></div>
           <div>Finanzamt Baden-Baden 36216/14616</div>
         </div>
       </div>
