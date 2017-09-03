389047efdf2eae01f411ac14121350c227f65d6f
firsttris <tristanteufel@googlemail.com>
2017-09-03 22:32:41 +0200

    update react-prototype

--- src/Routing.js
+++ src/Routing.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch
+    HashRouter,
+    Route,
+    Switch
 } from 'react-router-dom'
 import Development from './components/Development'
 import Consulting from './components/Consulting'
@@ -11,60 +11,62 @@ import LinkItem from './components/LinkItem'
 import NotFound from './components/NotFound'
 
 const fixedNavBarStyle = {
-  width: '100%',
-  position: 'fixed',
-  top: '0px',
-  zIndex: '10',
-  borderBottom: '1px solid #f00'
+    width: '100%',
+    position: 'fixed',
+    top: '0px',
+    zIndex: '10',
+    borderBottom: '1px solid #f00'
 };
 
 const ulStyle = {
-  listStyleType: 'none',
-  margin: '0',
-  padding: '0',
-  overflow: 'hidden',
-  backgroundImage: `url(${Paper})`
+    listStyleType: 'none',
+    margin: '0',
+    padding: '0',
+    overflow: 'hidden',
+    backgroundImage: `url(${Paper})`
 };
 
 class Routing extends Component {
-  constructor() {
-    super();
-    this.state = {
-      linkitem: 0
+    constructor() {
+        super();
+        this.state = {
+            linkitem: 0
+        }
     }
-  }
 
-  onClick(index, e) {
-    this.setState({ linkitem: index });
-  }
+    onClick(index, e) {
+        this.setState({linkitem: index});
+    }
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <div style={fixedNavBarStyle}>
-            <ul style={ulStyle}>
-              <div onClick={this.onClick.bind(this, 1)} >
-                <LinkItem focus={this.state.linkitem === 1} path="/" name="Entwicklung"></LinkItem>
-              </div>
-              <div onClick={this.onClick.bind(this, 2)} >
-                <LinkItem focus={this.state.linkitem === 2} path="/it-strategieberatung" name="Beratung"></LinkItem>
-              </div>
-              <div onClick={this.onClick.bind(this, 3)} >
-                <LinkItem focus={this.state.linkitem === 3} path="/softwareentwicklung" name="Software"></LinkItem>
-              </div>
-            </ul>
-          </div>
-          <Switch>
-            <Route exact path="/" component={Development} />
-            <Route path="/it-strategieberatung" component={Consulting} />
-            <Route path="/softwareentwicklung"  />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        return (
+            <HashRouter>
+                <div>
+                    <div style={fixedNavBarStyle}>
+                        <ul style={ulStyle}>
+                            <div onClick={this.onClick.bind(this, 1)}>
+                                <LinkItem focus={this.state.linkitem === 1} path="/" name="Entwicklung"/>
+                            </div>
+                            <div onClick={this.onClick.bind(this, 2)}>
+                                <LinkItem focus={this.state.linkitem === 2} path="/it-strategieberatung"
+                                          name="Beratung"/>
+                            </div>
+                            <div onClick={this.onClick.bind(this, 3)}>
+                                <LinkItem focus={this.state.linkitem === 3} path="/softwareentwicklung"
+                                          name="Software"/>
+                            </div>
+                        </ul>
+                    </div>
+                    <Switch>
+                        <Route exact path="/" component={Development}/>
+                        <Route path="/it-strategieberatung" component={Consulting}/>
+                        <Route path="/softwareentwicklung"/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+            </HashRouter>
+        );
+    }
 }
 
 export default Routing;
