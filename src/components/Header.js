3893b5a42ee934df2c4ae0acf84868e736209c44
tristanteufel@gmail.com <U9in0RnXPdM2>
2017-09-07 16:37:19 +0200

    feat(Github): Github integration

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,15 +4,17 @@ import {FormattedMessage} from 'react-intl';
 export default class Header extends Component {
   render() {
     return (
-      <div style={{position: 'absolute', top: '200px', width: '100%',}}>
-        <div className="p-3" style={{backgroundColor: '#4B4B4B', display: 'inline-block' }}>
-          <div style={{fontSize: '35px', letterSpacing:'8px',color:'#f00'}}>
-              <FormattedMessage id="HEADER_TITLE"/>
+      <div className="row" style={{position: 'absolute', top: '200px', width: '100%',}}>
+          <div className="col">
+              <div className="p-3" style={{backgroundColor: '#4B4B4B', display: 'inline-block' }}>
+                  <div style={{fontSize: '35px', letterSpacing:'8px',color:'#f00'}}>
+                      <FormattedMessage id="HEADER_TITLE"/>
+                  </div>
+                  <div className="pt-3" style={{fontSize: '15px', letterSpacing:'5px',color:'white'}}>
+                      <FormattedMessage id="HEADER_SUBTITLE"/>
+                  </div>
+              </div>
           </div>
-          <div className="pt-3" style={{fontSize: '15px', letterSpacing:'5px',color:'white'}}>
-              <FormattedMessage id="HEADER_SUBTITLE"/>
-          </div>
-        </div>
       </div>
     );
   }
--- src/components/pages/Github.js
+++ src/components/pages/Github.js
@@ -1,23 +1,47 @@
 import React, {Component} from 'react';
-import GitHub from 'github-api';
-// basic auth
-const gh = new GitHub({});
-
+import OldComputer from './../../../public/images/oldComputer2.jpg';
+import FullImage from './../FullImage';
 
 export default class Github extends Component {
 
     componentWillMount() {
-        const firsttris = gh.getUser('firsttris');
-        firsttris.getProfile()
-            .then(function({data: data}) {
-                console.log(data);
-            });
     }
 
     render() {
         return (
             <div>
-                <h1>GITFUCK</h1>
+                <FullImage image={OldComputer} height="150px"/>
+                <div className="container">
+                    <div className="form-group pt-3">
+                        <input type="search" className="form-control" id="Search" placeholder="Search"/>
+                    </div>
+                    <div className="list-group">
+                        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                            <div className="d-flex w-100 justify-content-between">
+                                <h5 className="mb-1">List group item heading</h5>
+                                <small>3 days ago</small>
+                            </div>
+                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                            <small>Donec id elit non mi porta.</small>
+                        </a>
+                        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                            <div className="d-flex w-100 justify-content-between">
+                                <h5 className="mb-1">List group item heading</h5>
+                                <small className="text-muted">3 days ago</small>
+                            </div>
+                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                            <small className="text-muted">Donec id elit non mi porta.</small>
+                        </a>
+                        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+                            <div className="d-flex w-100 justify-content-between">
+                                <h5 className="mb-1">List group item heading</h5>
+                                <small className="text-muted">3 days ago</small>
+                            </div>
+                            <p className="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+                            <small className="text-muted">Donec id elit non mi porta.</small>
+                        </a>
+                    </div>
+                </div>
             </div>
         );
     }
