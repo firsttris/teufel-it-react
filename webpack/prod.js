bcf5c6278a7f3b475d73fda65970f372d510168d
firsttris <tristanteufel@googlemail.com>
2017-09-09 21:08:35 +0200

    fix(CNAME): create cname file for github pages with custom domain

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,7 +1,16 @@
 const base = require('./base');
+const path = require('path');
+const fs = require('fs');
 const webpack = require('webpack');
 const BabiliPlugin = require('babili-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const distPath = path.resolve(__dirname, '../dist');
+
+fs.writeFile(path.join(distPath,'CNAME'), 'www.teufel-it.de', (err) => {
+    if (err) {
+        throw err;
+    }
+});
 
 base.module.rules.push(
     {
@@ -14,11 +23,11 @@ base.module.rules.push(
 );
 base.plugins.push(
     new webpack.optimize.CommonsChunkPlugin({
-        name: "vendor",
+        name: 'vendor',
         minChunks: function (module) {
             // This prevents stylesheet resources with the .css or .scss extension
             // from being moved from their original chunk to the vendor chunk
-            if(module.resource && (/^.*\.(css|scss)$/).test(module.resource)) {
+            if (module.resource && (/^.*\.(css|scss)$/).test(module.resource)) {
                 return false;
             }
             return module.context && module.context.indexOf("node_modules") !== -1;
@@ -28,7 +37,7 @@ base.plugins.push(
         'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new BabiliPlugin(),
-    new ExtractTextPlugin("styles.css")
+    new ExtractTextPlugin('styles.css')
 );
 base.devtool = 'source-map';
 module.exports = base;
